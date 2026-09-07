b0a29147c6ab55bda032fe976c22c40ac3478d31
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-09-07 11:05:32 +0200

    ja vel

--- Eksamen/2023_ordiner/exchange_rates.R
+++ Eksamen/2023_ordiner/exchange_rates.R
@@ -1,5 +1,6 @@
 
 library(docstring)
+library(readxl) #for å lese excel-filer
 #a Lager et tidsserieplott
 library(tidyverse)
 euro <- read.csv("euro.csv") 
@@ -18,3 +19,26 @@ task_c <- euro |> filter(year(t)>=2014)|>#begynner 1. januar 2014
   summarise(maks = max(rate), minst = min(rate)) #finner største og minste verdi, en totuppel i summarisen
 
 #' Vi får da et tidy format, med den tabbellen vi ønsker
+
+
+#d
+
+nb_rates <- read_xlsx("nb_rates.xlsx") #importerer norges banks styringsrente fra 1986 til 2023 
+
+nb_rates |> ggplot() + geom_line(aes(x = date, y = policy_rate)) + labs(x = "Tid", y = "Styringsrente", title = "Styringsrente over tid")
+
+
+#e
+colnames(euro) <- c("date","kurs")
+
+nb_rates <- nb_rates |>
+  filter(date > min(euro$date)) |> #begrenser til der vi har data for valutakursen, da dette er området det gir mening å jobbe med
+  arrange((date))  #gjør dem i stigende rekkefølge, slik at vi får samme format som for euro-datesettet
+
+
+print(nrow(euro)/12)
+print(nrow(nb_rates))
+
+
+View(euro)
+View(nb_rates)
